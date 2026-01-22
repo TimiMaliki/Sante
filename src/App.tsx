deb935dc0a32f9bc518c17ce01520d3b1c33126b
TimiMaliki <malikitimi@gmail.com>
2026-01-22 04:34:27 +0100

    added : Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import Navbar from "./Pages/Navbar";
+import { SelectedPage } from "./shared/type";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState("home")
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
   return ( 
     <div className="app bg-gray-20">
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
